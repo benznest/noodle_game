41a49374e688b401287e89a84807a9f892cceafd
benznest <benz7net@gmail.com>
2022-01-30 19:17:46 +0700

    change icon web.

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: MyGame.gameTitle,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.grey,
       ),
       home: const MyGamePage(),
     );
